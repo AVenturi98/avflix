f7998c28296a83e4e1a4649253e37c865068d998
AVenturi98 <lssndrventuri@gmail.com>
2025-02-14 22:36:21 +0100

    FIX dettails buttons modal

--- src/components/ImagesCollage.jsx
+++ src/components/ImagesCollage.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
-import Gallery from "react-photo-gallery";
 import Modal from "react-modal";
+
+// Icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight, faX } from "@fortawesome/free-solid-svg-icons";
 
 // Definiamo l'elemento principale dell'app per evitare il warning
 Modal.setAppElement("#root");
@@ -80,9 +83,16 @@ const CollageGallery = ({ images = [] }) => {
                 </div>
                 {selectedIndex !== null && (
                     <div style={{ position: "relative", width: "100%", height: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}>
-                        <button onClick={prevImage} style={{ position: "absolute", left: "10px" }}>&lt;</button>
+                        <button onClick={prevImage} style={{ position: "absolute", left: "10px" }} className="text-3xl hover:bg-gray-300 p-5 rounded-xl">
+                            <FontAwesomeIcon icon={faArrowLeft} />
+                        </button>
                         <img src={images[selectedIndex].src} alt="Selected" style={{ maxWidth: "100%", maxHeight: "100%", objectFit: "contain", borderRadius: "8px" }} />
-                        <button onClick={nextImage} style={{ position: "absolute", right: "10px" }}>&gt;</button>
+                        <button onClick={nextImage} style={{ position: "absolute", right: "10px" }} className="text-3xl hover:bg-gray-300 p-5 rounded-xl">
+                            <FontAwesomeIcon icon={faArrowRight} />
+                        </button>
+                        <button onClick={closeModal} className="hover:text-gray-300 p-3">
+                            <FontAwesomeIcon icon={faX} className="absolute top-2 right-2" />
+                        </button>
                     </div>
                 )}
             </Modal>
