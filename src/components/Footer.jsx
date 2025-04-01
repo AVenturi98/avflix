e43f6f5c651b69d45068e740114a9c1a20521ac7
AVenturi98 <lssndrventuri@gmail.com>
2025-04-01 14:58:09 +0200

    analytics packages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,11 +7,11 @@ export default function Footer() {
             <div className="border-t-4 border-sky-950 text-center py-4">
                 <p className="text-sm text-sky-950">
                     <FontAwesomeIcon icon={faCopyright} className="mr-2" />
-                    <span> AV studios. All rights reserved </span>
+                    <span> AVi studios. All rights reserved </span>
                 </p>
                 <br />
                 <a href="https://github.com/AVenturi98" className="hover:underline">
-                    <p className="text-gray-500">Create by <span className="text-sky-950">AV</span></p>
+                    <p className="text-gray-500">Create by <span className="text-sky-950">AVi</span></p>
                 </a>
 
             </div>
