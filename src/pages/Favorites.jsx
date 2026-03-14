19eed4b7887ca7f2e89794b2e2bacfd2eb9c6a95
AVenturi98 <lssndrventuri@gmail.com>
2026-03-14 04:44:21 +0100

    mex fav page

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export default function Favorites() {
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+            <h1 className="text-4xl font-bold mb-4">Favorites Page</h1>
+            <p className="text-lg text-gray-700">This is where your favorite items will be displayed.</p>
+            <p className="text-md text-gray-800 font-bold">WORK IN PROGRESS</p>
+            <p className="text-md text-gray-600">I'll see you soon</p>
+        </div>
+    );
+}
